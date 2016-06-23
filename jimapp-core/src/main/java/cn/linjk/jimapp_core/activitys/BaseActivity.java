3d085f95f1df5121400633aa6f2930b3a04aa0b1
Jim_WIndows <linjk121@163.com>
2016-06-23 12:40:32 +0800

    fixed javadoc error with utf-8 encoding

--- jimapp-core/src/main/java/cn/linjk/jimapp_core/activitys/BaseActivity.java
+++ jimapp-core/src/main/java/cn/linjk/jimapp_core/activitys/BaseActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 /**
  * Created by Jim on 2016/6/23.
- *
- * 与业务无关的基类，app应用开发的基类应继承此类进行扩展开发
  */
 public class BaseActivity extends Activity{
     @Override
--- jimapp-core/src/main/java/cn/linjk/jimapp_core/app/AppManager.java
+++ jimapp-core/src/main/java/cn/linjk/jimapp_core/app/AppManager.java
@@ -7,19 +7,17 @@ import java.util.Stack;
 /**
  * Created by LinJK on 6/23/16.
  *
- * 用于管理activity
  */
 public class AppManager {
 
-    private static Stack<Activity> stackActivity; //用于保存栈的activity以便于进行操作
+    private static Stack<Activity> stackActivity;
     private static AppManager      INSTANCE;
 
     private AppManager() {}
 
     /**
-     * 获取AppManager单例
      *
-     * @return AppManager单例对象
+     * @return AppManager
      */
     public static AppManager getAppManager() {
         if (null == INSTANCE) {
@@ -30,9 +28,8 @@ public class AppManager {
     }
 
     /**
-     * 添加activity到堆栈
      *
-     * @param activity 要压入堆栈的activity
+     * @param activity
      */
     public void addActivity(Activity activity) {
         if (null == stackActivity) {
@@ -43,9 +40,9 @@ public class AppManager {
     }
 
     /**
-     * 获取activity堆栈栈顶的activity
      *
-     * @return 当前activity堆栈栈顶的activity
+     *
+     * @return activity
      */
     public Activity currentActivity() {
         return stackActivity.lastElement();
