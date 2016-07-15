a8020dbff262e353c6b95a5fc563563bf808492f
Jim <linjk121@163.com>
2016-07-16 00:10:30 +0800

    implements method of HexStringToByteArray.

--- jimapp-core/src/main/java/cn/linjk/jimapp_core/utils/StringUtils.java
+++ jimapp-core/src/main/java/cn/linjk/jimapp_core/utils/StringUtils.java
@@ -1,5 +1,6 @@
 package cn.linjk.jimapp_core.utils;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -10,15 +11,57 @@ import java.util.regex.PatternSyntaxException;
  * @version 1.0
  */
 public class StringUtils {
+    /**
+     * Convert hex-string to byte[] array format.
+     * @param string
+     * @return
+     */
     public static byte[] HexStringToByteArray(String string) {
         String resultString = null;
 
         try {
+            /**remove the non-hex characteristic in the string parameter.*/
             Pattern regex = Pattern.compile("[^\\da-f]*",
                                             Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE);
+            Matcher regexMatcher = regex.matcher(string);
+
+            try {
+                resultString = regexMatcher.replaceAll("");
+
+                int bufferLength;
+                if ( (resultString.length() % 2) == 1 ) {
+                    bufferLength = resultString.length()/2 + 1;
+                }
+                else {
+                    bufferLength = resultString.length()/2;
+                }
+
+                byte[] buffer = new byte[bufferLength];
+                for (int i = 0; i < bufferLength-1; i++) {
+                    int tmp = Integer.parseInt(resultString.substring(2*i, 2*i+2), 16);
+                    buffer[i] = (byte)(tmp&0xFF);
+                }
+                if (bufferLength > 0) {
+                    int index = bufferLength - 1;
+                    int len   = resultString.length()%2 == 1 ? 1 : 2;
+                    int tmp   = Integer.parseInt(resultString.substring(2*index, 2*index+len), 16);
+
+                    buffer[bufferLength-1] = (byte)(tmp & 0xFF);
+                }
+
+                return buffer;
+            }
+            catch (IllegalArgumentException ex) {
+                ex.printStackTrace();
+            }
+            catch (IndexOutOfBoundsException ex) {
+                ex.printStackTrace();
+            }
         }
         catch (PatternSyntaxException ex) {
             ex.printStackTrace();
         }
+
+        return null;
     }
 }
--- jimapp-core/src/test/java/cn/linjk/jimapp_core/ExampleUnitTest.java
+++ jimapp-core/src/test/java/cn/linjk/jimapp_core/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package cn.linjk.jimapp_core;
 
 import org.junit.Test;
 
+import cn.linjk.jimapp_core.utils.StringUtils;
 import cn.linjk.jimapp_core.utils.TimeUtils;
 
 import static org.junit.Assert.*;
@@ -20,6 +21,14 @@ public class ExampleUnitTest {
         int curDayOfMonth = TimeUtils.curDayOfMonth();
         int curMonth      = TimeUtils.curMonth();
 
-        System.out.print("Today is: " + curMonth + "month" + curDayOfMonth + "day");
+        System.out.println("Today is: " + curMonth + "month" + curDayOfMonth + "day");
+    }
+
+    @Test
+    public void testStringUtils() {
+        byte[] temp = StringUtils.HexStringToByteArray("68123456789012680102");
+        for (byte tmp : temp) {
+            System.out.println(String.format("%02X", tmp));
+        }
     }
 }
